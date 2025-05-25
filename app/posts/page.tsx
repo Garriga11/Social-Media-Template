e0cec3e51dd466d36f7edef71435a01e0c266a7e
Garriga11 <garriga11@outlook.com>
2025-05-25 00:13:39 -0500

    commit

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -23,7 +23,9 @@ export default async function Home() {
                 select: {
                     id: true,
                     clerkId: true,
-                    name: true,
+                    firstName: true,
+                    lastName: true,
+                    profileImage: true,
                     email: true,
                 },
             },
@@ -35,9 +37,9 @@ export default async function Home() {
                 {/* Add your input fields or UI elements here */}
                 <input type="text" placeholder="Write your post..." className="border p-2 rounded w-full" />
                 <button className="mt-2 bg-blue-500 text-white py-1 px-4 rounded">Post</button>
-                {posts.map((post: { id: number; content: string | null; createdAt: Date; author: { name: string | null } }) => (
+                {posts.map((post: { id: number; content: string | null; createdAt: Date; author: { firstName: string; lastName: string } }) => (
                     <div key={post.id} className="p-4 border border-zinc-800 rounded mt-4">
-                        <h2 className="font-bold">{post.author.name || "Unknown Author"}</h2>
+                        <h2 className="font-bold">{`${post.author.firstName} ${post.author.lastName}`.trim() || "Unknown Author"}</h2>
                         <p className="mt-2">{post.content || "No content available"}</p>
                     </div>
                 ))}
@@ -46,14 +48,14 @@ export default async function Home() {
     }
     return (
         <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-                        <h2 className="font-bold">{posts.length > 0 ? posts[0].author.name || "Unknown Author" : "No Posts Available"}</h2>
+                        <h2 className="font-bold">{posts.length > 0 ? `${posts[0].author.firstName} ${posts[0].author.lastName}`.trim() || "Unknown Author" : "No Posts Available"}</h2>
             <PostInputs/>
             <div className="mt-8">
-                {posts.map((post: { id: number; content: string | null; createdAt: Date; author: { name: string | null } }) => (
+                {posts.map((post: { id: number; content: string | null; createdAt: Date; author: { firstName: string; lastName: string } }) => (
                     <div
                         key={post.id}
                         className="p-4 border border-zinc-800 rounded mt-4">
-                        <h2 className="font-bold">{post.author.name || "Unknown Author"}</h2>
+                        <h2 className="font-bold">{`${post.author.firstName} ${post.author.lastName}`.trim() || "Unknown Author"}</h2>
                         <p className="mt-2">{post.content || "No content available"}</p>
                     </div>
                 ))}
